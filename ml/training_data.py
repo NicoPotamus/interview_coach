b2d911af3f3603d5633120a2a9a0982e569016bd
BryanV245 <144383665+BryanV245@users.noreply.github.com>
2025-02-19 14:48:23 -0500

    file structure created in neural network

--- ml/training_data.py
+++ ml/training_data.py
@@ -4,7 +4,7 @@ from spacy.tokens import Span, DocBin
 import csv
 
 TEXTS = []
-with open("/Users/bryan/School/Spring 2025/Projects/InterviewCoach/interview_coach/ml/related_skills.csv", mode='r') as f:
+with open("./related_skills.csv", mode='r') as f:
     reader = csv.reader(f, delimiter=',')
     next(reader)  # Skip the first row
     for row in reader:
@@ -13,11 +13,11 @@ with open("/Users/bryan/School/Spring 2025/Projects/InterviewCoach/interview_coa
 # Load the spaCy model use the following command to download the model
 # python -m spacy download en_core_web_lg
 nlp = spacy.load("en_core_web_lg")
-docs = []
-for doc in nlp.pipe(TEXTS):
-    span= Span(doc, 0, len(doc), label= "SKILL")
-    doc.ents = [span]
-    docs.append(doc)
+# docs = []
+# for doc in nlp.pipe(TEXTS):
+#     span= Span(doc, 0, len(doc), label= "SKILL")
+#     doc.ents = [span]
+#     docs.append(doc)
 
-doc_bin = DocBin(docs=docs)
-doc_bin.to_disk("./train.spacy")
+# doc_bin = DocBin(docs=docs)
+# doc_bin.to_disk("./train.spacy")
