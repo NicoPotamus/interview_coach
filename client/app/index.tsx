f6c196319ff24457b1f55dd35c7e1c288f72aea8
Nico DeMilio <njdemilio@gmail.com>
2025-02-22 14:39:07 -0500

    style front end, navbar doesn't work on mobile nor is it pinned to bottom of screen atm

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,13 +1,65 @@
-import { Text, View, TextInput } from "react-native";
+import "./../global.css"
+import * as React from "react";
+import { Image } from "react-native";
+import { Banner, Button, TextInput } from "react-native-paper";
+import { View } from "react-native";
+import Navbar from "./components/navbar";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const [visible, setVisible] = React.useState(true);//Banner
+  const [text, setText] = React.useState("");//Search bar
+
+
+
   return (
-    <View className="flex-1 justify-center items-center bg-blue-50">
-      <Text className="text-3xl font-bold text-blue-800 mb-6">Enter a job title:</Text>
+    <View className="justify-center">
+      <Banner
+        visible={visible}
+        actions={[
+          {
+            label: "Continue",
+            onPress: () => setVisible(false),
+          },
+          {
+            label: "Do Beans",
+            onPress: () => setVisible(false),
+          },
+        ]}
+        icon={({ size }) => (
+          <Image
+            source={{
+              uri: "https://avatars3.githubusercontent.com/u/17571969?s=400&v=4",
+            }}
+            style={{
+              width: size,
+              height: size,
+            }}
+          />
+        )}
+      >
+        Welcome to InterViewCoach, we'll help you get that deam job you've
+        always wanted by identifying the skills the job market is looking for
+        for your desired role.
+      </Banner>
       <TextInput
-        className="h-12 border border-gray-300 rounded-full px-4 w-3/4 shadow-md"
-        placeholder="Job Title"
+        label="Job Title"
+        value={text}
+        onChangeText={(text) => setText(text)}
+        className="mb-4"
       />
+      <View className="items-center">
+        <View className="mt-4 w-1/2">
+          <Button
+            icon="database-search"
+            mode="contained"
+            onPress={() => console.log("Pressed")}
+          >
+            Search Skills
+          </Button>
+        </View>
+      </View>
+      <Navbar />
     </View>
   );
-}B
+}
--- ml/training_data.py
+++ ml/training_data.py
@@ -13,11 +13,11 @@ with open("./related_skills.csv", mode='r') as f:
 # Load the spaCy model use the following command to download the model
 # python -m spacy download en_core_web_lg
 nlp = spacy.load("en_core_web_lg")
-# docs = []
-# for doc in nlp.pipe(TEXTS):
-#     span= Span(doc, 0, len(doc), label= "SKILL")
-#     doc.ents = [span]
-#     docs.append(doc)
+docs = []
+for doc in nlp.pipe(TEXTS):
+    span= Span(doc, 0, len(doc), label= "SKILL")
+    doc.ents = [span]
+    docs.append(doc)
 
-# doc_bin = DocBin(docs=docs)
-# doc_bin.to_disk("./train.spacy")
+doc_bin = DocBin(docs=docs)
+doc_bin.to_disk("./train.spacy")
