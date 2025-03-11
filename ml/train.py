a89bcd117d70090817fb5cc96333b5b5bca250ce
Nico DeMilio <njdemilio@gmail.com>
2025-03-11 17:35:38 -0400

    cleaning

--- ml/train.py
+++
@@ -1,7 +0,0 @@
-import spacy
-from spacy.tokens import Span
-nlp = spacy.load('en_core_web_sm')
-doc8 = nlp("Hands/on world-class experience with Python and Django.")
-print(len(doc8))
-for token in doc8:
-    print(token.text, token.pos_)
