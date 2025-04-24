5840a1ca3713ad9962a9a36f554f15f931d8fb48
BryanV245 <144383665+BryanV245@users.noreply.github.com>
2025-04-24 00:56:19 -0400

    login and register working

--- client/app/login.tsx
+++ client/app/login.tsx
@@ -2,21 +2,31 @@ import { useState } from "react";
 import { View, Text, TextInput, Button, Alert } from "react-native";
 import { loginUser } from "@/lib/api";
 import { useRouter } from "expo-router";
-import * as SecureStore from "expo-secure-store";
+
+import { useEffect } from "react";
+
+import { saveToken } from "@/lib/storage";
 
 export default function Login() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+
   const handleLogin = async () => {
     try {
       const { access_token } = await loginUser(email, password);
-      await SecureStore.setItemAsync("token", access_token);
+      await saveToken("token", access_token);
       Alert.alert("Success", "Logged in!");
-      router.push("/"); // Redirect to home or dashboard
+      router.push("/"); 
     } catch (err: any) {
-      Alert.alert("Login Failed", err.message);
+      const errorMessage =
+        err?.response?.data?.detail ||
+        err?.message ||
+        JSON.stringify(err);
+    
+      Alert.alert("login Failed", errorMessage);
+      console.error("loginError", errorMessage);
     }
   };
 
