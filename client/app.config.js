d6d9c8ed2b4c3329eb955a85b78c8a25f31aaec8
BryanV245 <144383665+BryanV245@users.noreply.github.com>
2025-04-28 17:17:57 -0400

    tab name

--- client/app.config.js
+++ client/app.config.js
@@ -2,8 +2,8 @@ import "dotenv/config";
 
 export default {
   expo: {
-    name: "client",
-    slug: "client",
+    name: "Interview Coach",
+    slug: "Interview Coach",
     version: "1.0.0",
     extra: {
       apiUrl: process.env.EXPO_PUBLIC_API_URL,
